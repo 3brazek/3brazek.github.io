980aaaccebd62576402898d078fbc05f7d31bbdc
3brazek <92313564+3brazek@users.noreply.github.com>
2021-10-11 15:39:55 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 $db_url = "localhost";
-$db_name = ""; //database name
-$db_username = ""; //database account username
-$db_password=""; //database account password
+$db_name = "test-bef270"; //database name
+$db_username = "test-bef270"; //database account username
+$db_password="d8a3"; //database account password
 $epg_url = ""; //epg xml url (http://iptvmedia.live:8080/get.php?username=test-bef270&password=d8a3&type=m3u_plus&output=ts)
 $epg_valid_hours = "+12 hours"; //if database has epg available for less than 12 hours it will be updated. Alternative values: "+1 day", "+2 days", "+1 week" etc etc
 
